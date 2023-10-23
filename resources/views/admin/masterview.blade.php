576e9c623407a2344f4df72216813bf0477354dc
vivi <duong682@gmailcom>
2023-10-23 14:15:35 +0700

    update

--- resources/views/admin/masterview.blade.php
+++ resources/views/admin/masterview.blade.php
@@ -47,7 +47,7 @@
                     </a>    
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.html">
+                    <a class="nav-link" href="{{route('admin.index')}}">
                         <i class="mdi mdi mdi-account menu-icon"></i> 
                            <span class="menu-title">Customer</span>
                     </a>
@@ -101,7 +101,7 @@
                 </div>
             </div>
             <nav class="navbar col-lg-12 col-12 p-lg-0 fixed-top d-flex flex-row">
-                <div class="navbar-menu-wrapper d-flex align-items-stretch justify-content-between">
+                <div class="navbar-menu-wrapper d-flex align-items-  justify-content-between">
                     <a class="navbar-brand brand-logo-mini align-self-center d-lg-none" href="index.html"><img
                             src="{{asset('admin-assets')}}//images/logo-mini.svg" alt="logo" /></a>
                     <button class="navbar-toggler navbar-toggler align-self-center mr-2" type="button"
