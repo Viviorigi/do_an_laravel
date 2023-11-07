37ef8c03b53f9ffefdbd4c6c5cbe1ed92dfe1bfe
Viviorigi <duong682@gmail.com>
2023-11-07 19:29:22 +0700

    okok

--- app/Http/Controllers/Admin/AdminOrderController.php
+++ app/Http/Controllers/Admin/AdminOrderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\Order_detail;
+use DB;
 class AdminOrderController extends Controller
 {
     public function index() {
@@ -12,7 +14,7 @@ class AdminOrderController extends Controller
         return view('admin.order.order-index',compact('order'));
     }
     public function edit($id)  {
-        $order=Order::find($id)->first();
+        $order=Order::find($id);
        
         return view('admin.order.edit-order',compact('order'));
     }
@@ -25,5 +27,9 @@ class AdminOrderController extends Controller
             dd($th);
             return redirect()->back()->with('error','Cập nhật thất bại');
         }
+    }
+    public function detail($id)  {
+        $detail=Order_detail::where('order_id',$id)->get();
+        return view('admin.order.order-detail',compact('detail'));
     }
 }
---
+++ resources/views/admin/order/order-detail.blade.php
@@ -0,0 +1,55 @@
+@extends('admin.masterview')
+@section('title')
+    Order
+@endsection
+
+@section('main-content')
+    <div class="col-lg-12 mt-5 grid-margin stretch-card">
+        <br>
+
+        <div class="card mt-5">
+            <div class="table-responsive">
+                <div class="mt-1 d-flex">
+                   
+                </div>
+                <div class="m-2 d-flex justify-content-lg-between justify-content-md-around ">
+                    <a href="{{route('order.index')}}" class="btn btn-primary ml-3">Back</a>
+                </div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>STT</th>
+                            <th>OrderID</th>   
+                            <th>Product Name</th>   
+                            <th>Product Price</th>
+                            <th>Quantity</th>   
+                            <th>Subtotal</th>                
+                            <th>Created</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($detail as $item)
+                        <tr>
+                            <td>{{$loop->iteration}}</td>
+                            <td>{{$item->order_id}}</td>
+                            <td>{{$item->product->name}}</td>
+                            <td>{{number_format(($item->product->sale_price>0)
+                            ?$item->product->sale_price:$item->product->price)}}Đ</td>   
+                            <td>{{$item->quantity}}</td>         
+                            <td>{{number_format($item->total_price)}}Đ</td>
+                            <td>{{date("d/m/Y", strtotime($item->created_at))}}</td>
+                                                   
+                        </tr>
+                        @endforeach
+                        
+                      
+                    </tbody>
+                </table>
+
+               
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
